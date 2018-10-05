4138367a1b0957e7a90f5cc9830e1bd6acafdc3d
Christian O. Venegas <cvenegas@esri.com>
2018-10-05 14:27:26 -0700

    win: Add a missing winsock header and set the minimum to Windows 7
    A winsock header was missing which is needed for GDAL's proxy server.
    The _WIN32_WINNT version was also too low for WinUWP so the minimum set
    has been upped to Windows 7 which is the lowest platform that the
    runtime supports.

--- gdal/gcore/gdalclientserver.cpp
+++ gdal/gcore/gdalclientserver.cpp
@@ -32,7 +32,7 @@
   #ifdef _WIN32_WINNT
     #undef _WIN32_WINNT
   #endif
-  #define _WIN32_WINNT 0x0501
+  #define _WIN32_WINNT 0x0601
   #include <winsock2.h>
   #include <ws2tcpip.h>
   typedef SOCKET CPL_SOCKET;
